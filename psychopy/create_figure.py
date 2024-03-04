248d274f2b7884ecb306441aaa5e2b02b4dbbf18
dariusvalevicius <darius.valevicius@gmail.com>
2024-03-04 10:20:48 -0500

    updated figure creation

--- psychopy/create_figure.py
+++ psychopy/create_figure.py
@@ -4,46 +4,21 @@ import os
 import pandas as pd
 import seaborn as sns
 import statsmodels.api as sm
-import pickle
-from scipy.stats import gaussian_kde
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.spatial import cKDTree
-
 
 global pop_size
 pop_size = 9
+global max_iters
+max_iters = 10
 global stim_time
 stim_time = 5
-global washout_time
-washout_time = 1
 
+global data_types
+data_types = {
+        'eda': 10,
+        'hr': 5,
+        'temp': 5
+    }
 
-
-
-def plot_score(file):
-    data = np.loadtxt(file, delimiter=",")
-
-    m, n = data.shape
-
-    # Calculate means and standard deviations (for error bars)
-    means = np.mean(data, axis=1)
-    std_devs = np.std(data, axis=1)
-
-    # Plot the raw data points
-    plt.plot(data, 'o', alpha=0.5, color="black")
-
-    # Plot the means with error bars
-    plt.errorbar(np.arange(m), means, yerr=std_devs, fmt='o-', color='black')
-
-    plt.xticks(np.arange(m), (np.arange(m)+1).astype(int))
-    plt.yticks(np.arange(11))
-
-    # Customize the plot
-    plt.xlabel('Generation')
-    plt.ylabel('Values')
-    plt.title('Scores by generation')
-
-    # plt.show()
 
 def plot_both(file1, file2):
     data1 = np.loadtxt(file1, delimiter=",")[:,1:]
@@ -118,7 +93,6 @@ def plot_top_images(file):
     # Adjust layout to prevent overlap
     plt.tight_layout()
 
-
 def correlation_plot(file1, file2, condition):
     data1 = np.loadtxt(file1, delimiter=",")[:,1:]
     data1 = data1.reshape(data1.shape[0]*data1.shape[1])
@@ -154,8 +128,7 @@ def correlation_plot(file1, file2, condition):
     # Print the regression summary
     print(model.summary())
 
-
-def plot_eda(file1, file2):
+def plot_eda(file1, file2, freq):
 
     data = np.loadtxt(file1, delimiter=",")
     data = np.delete(data, np.arange(0, data.shape[0], pop_size), axis=0)
@@ -177,32 +150,20 @@ def plot_eda(file1, file2):
     # Generate data for each line
     for i in range(n_lines):    
 
-
-        # if data[i,20:20].max() - data[i,5:10].mean() > 0.01:
         if scr_scores[i] > 0:
             colors = colors_resp
             num_responses = num_responses + 1
-            # plt.plot(data_centered[i,:25], color=colors[i], alpha=0.25)
-
-        # elif data[i,30:70].mean() - data[i,:20].mean() > 0.01:
-        #     colors = "orange"
         else:
             colors = colors_no_resp
-            # plt.plot(data_centered[i,:25], color=colors[i], alpha=0.25)
-
-
 
         # Plot the line with varying color
-        plt.plot(data_centered[i,:25], color=colors[i], alpha=0.25)
+        plt.plot(data_centered[i,:freq*5], color=colors[i], alpha=0.25)
 
-    plt.vlines(4, ymin=data.min(), ymax=data.max(), linestyles='dotted', color='black')
-    plt.vlines(19, ymin=data.min(), ymax=data.max(), linestyles='dotted', color='black')
-    # plt.vlines(15, ymin=data.min(), ymax=data.max(), linestyles='dotted', color='red')
-    # plt.vlines(30, ymin=data.min(), ymax=data.max(), linestyles='dotted', color='red')
+    plt.vlines(freq*1, ymin=data.min(), ymax=data.max(), linestyles='dotted', color='black')
+    plt.vlines(freq*4, ymin=data.min(), ymax=data.max(), linestyles='dotted', color='black')
 
     # Annotate the graph with a text label
     plt.annotate(f'Num responses:\n{num_responses}/{n_lines}', xy=(0, 0), xytext=(0, data.max()/2))
-
 
 def plot_scr_by_trial(file):
     data = np.loadtxt(file, delimiter=",")
@@ -226,136 +187,74 @@ def plot_scr_by_trial(file):
     plt.xlabel('Trial')
     plt.ylabel('Values')
     plt.title('Scores by trial within generation')
+    
+def interpolate_whole_file(data, rate):
 
-def plot_umap(participant):
+    data['time'] = pd.to_datetime(data['time'], format="%H:%M:%S.%f")
 
-    data = np.empty((0, 80))
-    scr_scores = np.empty((0, pop_size-1))
-    ratings = np.empty((0, pop_size-1))
+    values = data['value']
+    time_differences = data['time'] - data['time'].iloc[0]
+    timestamps = time_differences.dt.total_seconds().astype(float).values.tolist()
 
-    for condition in ["selfreport", "scr", "control"]:
-        root = f"data/{participant}/{condition}"   
+    # Fixed timestep for the curve
+    fixed_timestep = 1/rate  # Choose your desired timestep
 
-        # folder = f"{root}/figures/"
-        # if not os.path.exists(folder):
-        #     os.mkdir(folder)
+    # Interpolate data points
+    interp_func = np.interp(np.arange(timestamps[0], timestamps[-1]+fixed_timestep, fixed_timestep), timestamps, values)
 
-        data_new = np.loadtxt(f"{root}/embeddings.txt", delimiter=",")
-        data_new = np.delete(data_new, np.arange(0, data_new.shape[0], pop_size), axis=0)
-        print(data_new.shape)
+    return interp_func
 
-        data = np.vstack((data, data_new))
-        print(data.shape)
-        scr_scores = np.vstack((scr_scores, np.loadtxt(f"{root}/scr_scores.txt", delimiter=",")[:,1:]))
-        print(scr_scores.shape)
-        ratings = np.vstack((ratings, np.loadtxt(f"{root}/ratings.txt", delimiter=",")[:,1:]))
 
-    scr_scores = scr_scores.flatten()
-    ratings = ratings.flatten()
-
-    # Load the UMAP model from the pickle file
-    with open('models/umap.pkl', 'rb') as file:
-        umap_model = pickle.load(file)
-
-    # Transform your data using the UMAP model
-    umap_transformed = umap_model.transform(data)
-
-    # Plot the transformed vectors in the UMAP space
-    plt.scatter(umap_transformed[:, 0], umap_transformed[:, 1], c=ratings, cmap='viridis', alpha=0.6)
-    plt.title('UMAP Visualization: Ratings')
-    plt.xlabel('UMAP Dimension 1')
-    plt.ylabel('UMAP Dimension 2')
-    plt.colorbar(label='Rating')
+def plot_block(participant, condition, data_type):
+    df = pd.read_csv(f'data/{participant}/{condition}/{data_type}_data.txt', header=None, names=["value","time"], delimiter=',')
+    series = interpolate_whole_file(df, data_types[data_type])
+    # print(hr)
+    plt.plot(series)
+    plt.title(f"Participant {participant}, Condition: {condition}: {data_type}")
     plt.show()
 
-    # Plot the transformed vectors in the UMAP space
-    plt.scatter(umap_transformed[:, 0], umap_transformed[:, 1], c=scr_scores, cmap='viridis', alpha=0.6)
-    plt.title('UMAP Visualization: SCR')
-    plt.xlabel('UMAP Dimension 1')
-    plt.ylabel('UMAP Dimension 2')
-    plt.colorbar(label='SCR Peak')
-    plt.show()
+def interpolate_data(data, timings, rate):
+    # # Example data with timestamps and values
+    # timestamps = [0, 2, 5, 7]  # Example timestamps
+    # values = [10, 15, 20, 25]   # Example values
 
-    x = umap_transformed[:, 0]
-    y = umap_transformed[:, 1]
-    magnitudes = ratings
+    output = np.zeros((max_iters*pop_size, stim_time*rate))
 
-    points = np.column_stack((x, y))
-    grid_size = 40
+    data['time'] = pd.to_datetime(data['time'], format="%H:%M:%S.%f")
+    timings['start_time'] = pd.to_datetime(timings['start_time'], format="%H:%M:%S.%f")
+    timings['end_time'] = pd.to_datetime(timings['end_time'], format="%H:%M:%S.%f")
 
-    # Create an evenly spaced grid over the XY plane
-    x_min, y_min = np.min(points, axis=0)
-    x_max, y_max = np.max(points, axis=0)
-    x_grid = np.linspace(-20, 20, grid_size)
-    y_grid = np.linspace(-20, 20, grid_size)
-     # Initialize arrays to store grid points and their corresponding magnitudes
-    grid_points = np.zeros((grid_size, grid_size, 2))
-    grid_magnitudes = np.zeros((grid_size, grid_size))
+    for i in range(10):
+        for j in range(9):
+            # print(f"Doing trial {i*pop_size + j}/{max_iters*pop_size}")
+            trial_times = timings[(timings['generation'] == i) & (timings['trial'] == j)]
+            if len(trial_times) > 1:
+                trial_times = trial_times.tail(1)
+            
+            start_time = trial_times.iloc[0]['start_time']
+            end_time = trial_times.iloc[0]['end_time']
 
-    # Iterate over each point and assign it to the corresponding grid cell
-    for i in range(grid_size):
-        for j in range(grid_size):
-            # Define the boundaries of the current grid cell
-            x_min_cell = x_grid[i]
-            x_max_cell = x_grid[i+1] if i < grid_size - 1 else x_max
-            y_min_cell = y_grid[j]
-            y_max_cell = y_grid[j+1] if j < grid_size - 1 else y_max
-
-            # Find points within the current grid cell
-            mask = np.logical_and.reduce((
-                points[:, 0] >= x_min_cell,
-                points[:, 0] <= x_max_cell,
-                points[:, 1] >= y_min_cell,
-                points[:, 1] <= y_max_cell
-            ))
-            points_in_cell = points[mask]
-            magnitudes_in_cell = magnitudes[mask]
-
-            # Compute average magnitude for points in the cell
-            if len(points_in_cell) > 0:
-                average_magnitude = np.mean(magnitudes_in_cell)
-            else:
-                average_magnitude = 0 #np.nan  # No points in the cell
-
-            # Store grid point and average magnitude
-            grid_points[i, j] = [(x_min_cell + x_max_cell) / 2, (y_min_cell + y_max_cell) / 2]
-            grid_magnitudes[i, j] = average_magnitude
-
-    grid_points = grid_points.reshape(-1, 2)
-    averages = grid_magnitudes.ravel()
+            trial_data = data[(data['time'] >= start_time) & (data['time'] <= end_time)]
+            if len(trial_data) == 0:
+                trial_data = data[data['time'] <= end_time].tail(1)
 
 
-    # Calculate KDE with magnitudes as weights
-    kde = gaussian_kde([grid_points[:,0], grid_points[:,1]], weights=averages)
+            values = trial_data['value']
+            time_differences = trial_data['time'] - trial_data['time'].iloc[0]
+            timestamps = time_differences.dt.total_seconds().astype(float).values.tolist()
+            # print(timestamps)
 
-    # Define grid
-    # x_grid = np.linspace(-20, 20, 100)
-    # y_grid = np.linspace(-20, 20, 100)
-    X, Y = np.meshgrid(x_grid, y_grid)
-    Z = kde.evaluate([X.ravel(), Y.ravel()]).reshape(X.shape)
+            # Fixed timestep for the curve
+            fixed_timestep = 1/rate  # Choose your desired timestep
 
-    # Plot KDE
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(Z, cmap='viridis', alpha=0.8)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('2D Kernel Density Estimation with Magnitudes')
-    plt.show()
-
-    # Plot KDE in 3D
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(X, Y, Z, cmap='viridis')
-
-    # # Set labels and title
-    # ax.set_xlabel('X')
-    # ax.set_ylabel('Y')
-    # ax.set_zlabel('Density')
-    # ax.set_title('3D Kernel Density Estimation with Magnitudes')
-
-    # plt.show()
+            # Interpolate data points
+            interp_func = np.interp(np.arange(0, stim_time, fixed_timestep), timestamps, values)
 
 
+            output[i*pop_size + j, :] = interp_func
+            # print(interp_func)
+                
+    return output
 
 
 
@@ -384,39 +283,96 @@ def create_all(participant, condition, show, save):
     if save: plt.savefig(f"{root}/figures/scores_fig.png")
     plt.show() if show else plt.clf()
 
-    plot_both(f"{root}/ratings.txt", f"{root}/scr_means.txt")
-    plt.title(f'Image evolution: {condition} using scr_means')
+    plot_both(f"{root}/ratings.txt", f"{root}/eda_means.txt")
+    plt.title(f'Image evolution: {condition} with EDA')
     if save: plt.savefig(f"{root}/figures/means_fig.png")
     plt.show() if show else plt.clf()
 
-    plot_eda(f"{root}/scr_data.txt", f"{root}/scr_scores.txt")
+    plot_both(f"{root}/ratings.txt", f"{root}/hr_means.txt")
+    plt.title(f'Image evolution: {condition} with HR')
+    if save: plt.savefig(f"{root}/figures/means_fig.png")
+    plt.show() if show else plt.clf()
+
+    # plot_both(f"{root}/ratings.txt", f"{root}/temp_means.txt")
+    # plt.title(f'Image evolution: {condition} with TEMP')
+    # if save: plt.savefig(f"{root}/figures/means_fig.png")
+    # plt.show() if show else plt.clf()
+
+    plot_eda(f"{root}/eda_data_interp.txt", f"{root}/scr_scores.txt", 10)
     plt.title('EDA data')
     if save: plt.savefig(f'{root}/figures/eda_data.png')
     plt.show() if show else plt.clf()
+
+    # plot_eda(f"{root}/hr_data_interp.txt", f"{root}/scr_scores.txt", 5)
+    # plt.title('HR data')
+    # if save: plt.savefig(f'{root}/figures/eda_data.png')
+    # plt.show() if show else plt.clf()
+
+    # plot_eda(f"{root}/temp_data_interp.txt", f"{root}/scr_scores.txt", 5)
+    # plt.title('TEMP data')
+    # if save: plt.savefig(f'{root}/figures/eda_data.png')
+    # plt.show() if show else plt.clf()
 
     plot_scr_by_trial(f"{root}/scr_scores.txt")
     if save: plt.savefig(f'{root}/figures/scr_by_trial.png')
     plt.show() if show else plt.clf()
 
+def interpolate_all(participant, condition):
 
+    for data_type, rate in data_types.items():
+        root = f"data/{participant}/{condition}"
+        data = pd.read_csv(f'{root}/{data_type}_data.txt', header=None, names=["value", "time"], delimiter=',')
+        timings = pd.read_csv(f'{root}/trial_timings.txt', header=0, delimiter=',')
 
+        interp_data = interpolate_data(data, timings, rate)
+        np.savetxt(f'{root}/{data_type}_data_interp.txt', interp_data, delimiter=',')
+
+        means = np.mean(interp_data[:,1*rate:4*rate], axis=1).reshape(max_iters, pop_size)
+        np.savetxt(f'{root}/{data_type}_means.txt', means, delimiter=',')
 
 
 if __name__ == "__main__":
 
-    # create_all(50, "selfreport", show=True, save=False)
-    # create_all(51, "scr", show=True, save=False)
-    # create_all(52, "control", show=True, save=False)
-    # create_all(675972, "scr", show=True, save=False)
-    # create_all(69630, "scr", show=True, save=False)
-    # create_all(810175, "scr", show=True, save=False)
-    # create_all(701338, "scr", show=True, save=False)
-    # create_all(8193, "scr", show=True, save=True)
-    # create_all(665548, "scr", show=True, save=True)
-    create_all(831467, "control", show=True, save=True)
-    create_all(831467, "selfreport", show=True, save=True)
-    create_all(831467, "scr", show=True, save=True)
+    # interpolate_all(2, "selfreport")
+    create_all(2, "scr", show=True, save=False)
+
+    # plot_block(1, "control", "eda")
+
 
     # plot_umap(50)
     # # if save: plt.savefig(f'{root}/figures/umap.png')
     # plt.show()
+
+
+
+
+
+
+
+
+
+
+# def plot_score(file):
+#     data = np.loadtxt(file, delimiter=",")
+
+#     m, n = data.shape
+
+#     # Calculate means and standard deviations (for error bars)
+#     means = np.mean(data, axis=1)
+#     std_devs = np.std(data, axis=1)
+
+#     # Plot the raw data points
+#     plt.plot(data, 'o', alpha=0.5, color="black")
+
+#     # Plot the means with error bars
+#     plt.errorbar(np.arange(m), means, yerr=std_devs, fmt='o-', color='black')
+
+#     plt.xticks(np.arange(m), (np.arange(m)+1).astype(int))
+#     plt.yticks(np.arange(11))
+
+#     # Customize the plot
+#     plt.xlabel('Generation')
+#     plt.ylabel('Values')
+#     plt.title('Scores by generation')
+
+#     # plt.show()
